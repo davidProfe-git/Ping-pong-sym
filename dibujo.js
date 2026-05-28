72e7719e3f1021b67fd4e44f384f40d628fa67c9
Miller Mora <millermora2022@gmail.com>
2026-05-28 12:29:33 -0500

    se realizo el diseño de una figura en la etiqueta canva

--- dibujo.js
+++ dibujo.js
@@ -1,9 +1,30 @@
 let tablero = document.getElementById('tablero')
 let lienzo = tablero.getContext('2d')
 
-lienzo.beginPath() //inicializo el dibujo
-lienzo.strokeStyle = "#7DF527"
-lienzo.moveTo(0, 0) // punto A coordenadas
-lienzo.lineTo (304,304) // punt B coordenadas
-lienzo.stroke()
-lienzo.closePath()
+// lienzo.beginPath() //inicializo el dibujo
+// lienzo.strokeStyle = "#7DF527"
+// lienzo.moveTo(0, 0) // punto A coordenadas
+// lienzo.lineTo (30,300) // punt B coordenadas
+// lienzo.stroke()
+// lienzo.closePath()
+
+for (let y = 0; y < 300 ; y+=10){
+
+    lienzo.beginPath() //inicializo el dibujo
+    lienzo.strokeStyle = "#00ffc8"
+    
+    lienzo.moveTo(0, y) // punto A coordenadas
+    lienzo.lineTo (300 - y, 0) // punt B coordenadas
+    
+    lienzo.moveTo(300, y) // punto A coordenadas
+    lienzo.lineTo (300 - y, 300) // punt B coordenadas
+    
+    lienzo.moveTo(0, y) // punto A coordenadas
+    lienzo.lineTo (y+10,300) // punt B coordenadas
+    
+    lienzo.moveTo(300, y) // punto A coordenadas
+    lienzo.lineTo (y+10,0) // punt B coordenadas
+
+    lienzo.stroke()
+    lienzo.closePath()
+}
