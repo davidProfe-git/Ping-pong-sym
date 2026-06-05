afa71402dff8080870baa4dc17f948361374355a
MillerMora <millermora2022@gmail.com git config --global user.name MillerMora>
2026-06-05 17:57:35 -0500

    se realizo la funcion para realizar las lineas de diferentes colores

--- dibujo.js
+++ dibujo.js
@@ -8,23 +8,20 @@ let lienzo = tablero.getContext('2d')
 // lienzo.stroke()
 // lienzo.closePath()
 
-for (let y = 0; y < 300 ; y+=10){
-
+function dibujarLineas(color, x1,y1,x2,y2){
     lienzo.beginPath() //inicializo el dibujo
-    lienzo.strokeStyle = "#00ffc8"
+    lienzo.strokeStyle = color
     
-    lienzo.moveTo(0, y) // punto A coordenadas
-    lienzo.lineTo (300 - y, 0) // punt B coordenadas
+    lienzo.moveTo(x1, y1) // punto A coordenadas
+    lienzo.lineTo (x2, y2) // punt B coordenadas
     
-    lienzo.moveTo(300, y) // punto A coordenadas
-    lienzo.lineTo (300 - y, 300) // punt B coordenadas
-    
-    lienzo.moveTo(0, y) // punto A coordenadas
-    lienzo.lineTo (y+10,300) // punt B coordenadas
-    
-    lienzo.moveTo(300, y) // punto A coordenadas
-    lienzo.lineTo (y+10,0) // punt B coordenadas
-
     lienzo.stroke()
     lienzo.closePath()
+}
+
+for (let y = 0; y < 300 ; y+=10){   
+    dibujarLineas("#07ff03", 0, y, 300 - y, 0)
+    dibujarLineas("#ff0303", 300, y, 300 - y, 300)
+    dibujarLineas("#9415c6", 0, y, y + 10, 300)
+    dibujarLineas("#156dc6", 300, y, y + 10, 0)
 }
