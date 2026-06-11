9af4b9ca78aa983e0055f78e3733ba57b86dc000
AndresReina <andresfeliperv89@gmail.com>
2026-06-11 12:09:45 -0500

    se hizo movimiento de los palos y un poco la pelota

--- dibujos.js
+++ dibujos.js
@@ -1,38 +1,60 @@
-let tablero = document.getElementById("tablero");
-let lienzo = tablero.getContext("2d");
+let canvas = document.getElementById("canvas");
+let tablero = canvas.getContext("2d");
+let pelota = { x: 100, y: 500 };
+tablero.fillStyle = "white";
 
-for (i = 0; i < 300; i += 10) {
-  lienzo.beginPath(); //inizia el dibujo
-  lienzo.strokeStyle = "Red"; //le doy color al trazo
-  lienzo.moveTo(0, i); //coordenadas de el linzo
-  lienzo.lineTo(i + 10, 300); //hasta donde va el trazo
-  lienzo.stroke();
-  lienzo.closePath();
-} //abajo izquierda
+//aca hacemos el circulo
+tablero.arc(90, 65, 10, 0, Math.PI * 2);
+tablero.fill();
+dibujarlinea();
+addEventListener;
 
-for (i = 0; i < 300; i += 10) {
-  lienzo.beginPath(); //inizia el dibujo
-  lienzo.strokeStyle = "Blue"; //le doy color al trazo
-  lienzo.moveTo(i, 0); //coordenadas de el linzo
-  lienzo.lineTo(300, i + 10); //hasta donde va el trazo
-  lienzo.stroke();
-  lienzo.closePath();
-} // arriba derecha
+function dibujarlinea() {
+  for (i = 0; i <= 500; i = i + 15) {
+    tablero.fillRect(347.5, i, 5, 10);
+  }
+}
 
-for (i = 0; i < 300; i += 10) {
-  lienzo.beginPath(); //inizia el dibujo
-  lienzo.strokeStyle = "#77df62"; //le doy color al trazo
-  lienzo.moveTo(300 - i, 0); //coordenadas de el linzo
-  lienzo.lineTo(0, 0 + i); //hasta donde va el trazo
-  lienzo.stroke();
-  lienzo.closePath();
-} // arriba izquierda
+mueveRaqueta();
 
-for (i = 0; i < 300; i += 10) {
-  lienzo.beginPath(); //inizia el dibujo
-  lienzo.strokeStyle = "#ce1db6"; //le doy color al trazo
-  lienzo.moveTo(300 - i, 300); //coordenadas de el linzo
-  lienzo.lineTo(300, 0 + i); //hasta donde va el trazo
-  lienzo.stroke();
-  lienzo.closePath();
-} // arriba izquierda
+let RaquetaY = 150;
+
+function mueveRaqueta() {
+  document.addEventListener("keydown", function (tecla) {
+    if (tecla.key == "ArrowUp") {
+      RaquetaY = RaquetaY - 5;
+      dibujarRaqueta();
+      dibujarlinea();
+      tablero.fillRect(655, 25, 20, 100);
+    }
+  });
+
+  document.addEventListener("keydown", function (tecla) {
+    if (tecla.key == "ArrowDown") {
+      RaquetaY = RaquetaY + 5;
+      dibujarRaqueta();
+      dibujarlinea();
+      tablero.fillRect(655, 25, 20, 100);
+    }
+  });
+}
+
+function dibujarRaqueta() {
+  tablero.clearRect(0, 0, 700, 500); //deja en blanco todo
+  if (RaquetaY < 0) {
+    RaquetaY = 0;
+  }
+  if (RaquetaY > 400) {
+    RaquetaY = 400;
+  }
+  tablero.fillRect(25, RaquetaY, 20, 100); //pinta la raqueta
+}
+
+function dibujarPelota() {
+  tablero.clearRect(0, 0, 700, 400);
+  tablero.beginPath();
+  pelota.x = pelota.x + 5;
+  pelota.y = pelota.y - 7;
+  tablero.arc(pelota.x, pelota.y, 10, 0, Math.PI * 2);
+  tablero.fill();
+}
